586e12a3175da6a32f0d2b8facbcd30025610824
Matheus Muniz <mmuniz605@gmail.com>
2023-10-08 17:12:39 -0300

    feat: create class Produto

---
+++ src/main/java/com/mtmuniz/set/Ordenacao/Produto.java
@@ -0,0 +1,69 @@
+package com.mtmuniz.set.Ordenacao;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+public class Produto implements Comparable<Produto>{
+    private long codigo;
+    private String nome;
+    private double preco;
+    private int quatidade;
+
+    public Produto(long codigo, String nome, double preco, int quatidade) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+        this.quatidade = quatidade;
+    }
+
+    @Override
+    public int compareTo(Produto p) {
+        return nome.compareToIgnoreCase(p.getNome());
+    }
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuatidade() {
+        return quatidade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Produto produto)) return false;
+        return getCodigo() == produto.getCodigo();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCodigo());
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "codigo=" + codigo +
+                ", nome='" + nome + '\'' +
+                ", preco=" + preco +
+                ", quatidade=" + quatidade +
+                '}';
+    }
+}
+
+class ComparatorPorPreco implements Comparator<Produto> {
+
+    @Override
+    public int compare(Produto p1, Produto p2) {
+        return Double.compare(p1.getPreco(), p2.getPreco());
+    }
+}
